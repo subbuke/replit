2d1016b8c1ab3e02b4e62b7e9cfd118e094b9c1e
subramanyam <141896120+subbuke@users.noreply.github.com>
2025-03-14 07:18:58 +0000

    second commit

--- Backend/index.js
+++ Backend/index.js
@@ -1,2 +1,18 @@
+const express = require("express");
 
 
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("server is running");
+})
+
+app.get("/test", (req, res) => {
+  res.send("In test route")
+})
+
+const PORT = process.env.PORT || 5002;
+
+app.listen(PORT, () => {
+  console.log("server is running at "+ PORT);
+})
